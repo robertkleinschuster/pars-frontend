b9b8d38849b1360cabe7a20821e1047fc772c9a9
robertkleinschuster <robert.kleinschuster@niceshops.com>
2020-12-30 14:02:34 +0100

    js and css defer, loading polyfill

--- config/autoload/bundles.global.php
+++ config/autoload/bundles.global.php
@@ -3,11 +3,19 @@ return [
     'bundles' => [
         [
             'type' => 'js',
+            'output' => 'script-insertion.js',
+            'sources' => [
+                __DIR__ . '/../../bundles/js/script-insertion.js',
+            ]
+        ],
+        [
+            'type' => 'js',
             'output' => 'frontend-bundle.js',
             'sources' => [
                 __DIR__ . '/../../bundles/js/01-jquery.min.js',
                 __DIR__ . '/../../bundles/js/02-bootstrap.min.js',
                 __DIR__ . '/../../bundles/js/04-popper.min.js',
+                __DIR__ . '/../../bundles/js/loading-attribute-polyfill.min.js',
             ]
         ],
         [
