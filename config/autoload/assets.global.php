2ca6368a15400190dd3284344355b41ff0a54514
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-01-06 17:21:10 +0100

    icons

--- config/autoload/assets.global.php
+++ config/autoload/assets.global.php
@@ -2,7 +2,21 @@
 return [
     'assets' => [
         'list' => [
-            [],
+            [
+                'root' => __DIR__ . '/../../assets/icons',
+                'output' => 'frontend/assets/icons/speedometer.svg',
+                'source' => 'speedometer.svg'
+            ],
+            [
+                'root' => __DIR__ . '/../../assets/icons',
+                'output' => 'frontend/assets/icons/bolt.svg',
+                'source' => 'bolt.svg'
+            ],
+            [
+                'root' => __DIR__ . '/../../assets/icons',
+                'output' => 'frontend/assets/icons/distance.svg',
+                'source' => 'distance.svg'
+            ],
         ]
     ]
 ];
