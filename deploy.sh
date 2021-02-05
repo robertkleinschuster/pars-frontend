e5c41cfc38d0dc8e86605eed680792a806e13972
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-02-05 14:19:09 +0100

    deploy.sh update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 export PATH=/opt/plesk/php/7.4/bin:$PATH:$HOME/bin
-php /usr/lib64/plesk-9.0/composer.phar update --no-dev --no-interaction &> deploy.log
-php /usr/lib64/plesk-9.0/composer.phar install --no-dev --no-interaction &> deploy.log
+if [ -d "vendor" ]; then
+  php /usr/lib64/plesk-9.0/composer.phar update --no-dev --no-interaction &>deploy.log
+else
+  php /usr/lib64/plesk-9.0/composer.phar install --no-dev --no-interaction &>deploy.log
+fi
