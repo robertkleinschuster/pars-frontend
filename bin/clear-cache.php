02af642ee860e0ff66b43ee4f035f9b267abbe08
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-03-01 23:57:47 +0100

    clear cache after deployment

--- bin/clear-cache.php
+++ bin/clear-cache.php
@@ -14,6 +14,15 @@ require 'vendor/autoload.php';
 
     $cache = new \Pars\Core\Deployment\Cache($container->get('config'), $adapter);
     $cache->setTranslator($translator);
+    $now = new DateTime();
+    $cache->removeOption(\Pars\Core\Deployment\Cache::OPTION_RESET_OPCACHE);
+    $cache->removeOption(\Pars\Core\Deployment\Cache::OPTION_CLEAR_IMAGES);
+    $cache->removeOption(\Pars\Core\Deployment\Cache::OPTION_CLEAR_ASSETS);
+    $cache->removeOption(\Pars\Core\Deployment\Cache::OPTION_CLEAR_CACHE_POOL);
+    $cache->removeOption(\Pars\Core\Deployment\Cache::OPTION_CLEAR_CONFIG);
+    if ($now->format('H:i') != '00:00') {
+        $cache->removeOption(\Pars\Core\Deployment\Cache::OPTION_CLEAR_BUNDLES);
+    }
     $cache->clear();
     echo 'Cache Cleared';
 })();
